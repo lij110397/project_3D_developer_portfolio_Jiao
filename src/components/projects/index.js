4d79d9d5f9eeb03a28639e4231b3026a7482b07d
lij110397 <lij110397@163.com>
2024-06-19 19:39:02 +0800

    navbarProject path change

--- src/components/projects/index.js
+++ src/components/projects/index.js
@@ -1,5 +1,5 @@
 import HeroProject from "./HeroProject";
 import BlogList from "./BlogList";
 import Pagination from "./Pagination";
-import NavbarProject from "./NavBarProject";
+import NavbarProject from "./NavbarProject";
 export { HeroProject,BlogList, Pagination, NavbarProject };
